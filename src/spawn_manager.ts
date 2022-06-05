730b106ccd935828d88100c9bcd3a4e48ba12931
skyser2003 <shjang1992@gmail.com>
2022-06-06 03:31:35 +0900

    Spawn harvester if energy full

--- src/spawn_manager.ts
+++ src/spawn_manager.ts
@@ -73,7 +73,7 @@ export class SpawnManager {
         if (numHarvesters === 0) {
             this.spawnHarvester(currentEnergy);
         } else {
-            if (numHarvesters < 4) {
+            if (numHarvesters < 4 || maxEnergy == currentEnergy) {
                 this.spawnHarvester(maxEnergy);
             }
 
